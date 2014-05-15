3bb121c0a7a72069ac95ff51981f4b176280f855
Yves <Yves@YvesStuder-PC.home>
2014-05-15 17:31:12 +0200

    Chat support adjusted

--- src/Engine/Game.java
+++ src/Engine/Game.java
@@ -108,6 +108,10 @@ public class Game
 
     public void finish()
     {
+        if (opponent != null)
+        {
+            opponent.sendMessage(new DataTransport("Opponent closed the game instance"));
+        }
         lobby.destroyOldGame();
     }
 
