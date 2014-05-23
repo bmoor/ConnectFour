6974f290141d70a6879e28da56cedeb7ef32c172
Yves <Yves@YvesStuder-PC.home>
2014-05-23 17:43:24 +0200

    AI improved

--- src/Engine/Ghost.java
+++ src/Engine/Ghost.java
@@ -6,6 +6,9 @@
 package Engine;
 
 import Engine.GameState.State;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
 import java.util.Random;
 
 /**
@@ -36,7 +39,7 @@ public class Ghost
         int z;
         do
         {
-            z =randomGenerator.nextInt(field.getYsize()+1);
+            z = randomGenerator.nextInt(field.getYsize() + 1);
 //            if (z == 0)
 //            {
 //                // this code is needed to pimp the random-generator. 
@@ -54,7 +57,18 @@ public class Ghost
 //            }
         }
         while (field.getStone(field.getYsize() - 1, z) != State.EMPTY);
-        System.out.println("Random-Zahl" + z);
+//        String eingabe = "";
+//        try
+//        {
+//            InputStreamReader isr = new InputStreamReader(System.in);
+//            BufferedReader br = new BufferedReader(isr);
+//            eingabe = br.readLine();
+//            z = Integer.parseInt(eingabe);
+//        }
+//        catch (IOException e)
+//        {
+//        }
+        System.out.println("Random-Zahl = " + z);
         return z;
     }
 
@@ -119,7 +133,7 @@ public class Ghost
     }
 
     /**
-     *
+     * 
      * @author Yves Studer
      * @param field representation of the current field
      * @param stone owner of the stone
@@ -145,51 +159,82 @@ public class Ghost
     {
         return field.getStone(y, x) == State.EMPTY;
     }
-
+    
     /**
-     * Create a random number from two given possibilities. When the random turn
-     * provide a win for the human in the next turn, it will calculate an other
-     * random number.
-     *
+     * Check the position, if the human can win on the next turn, the method will
+     * return false
+     * 
      * @param field representation of the current field
-     * @param first first number
-     * @param second second number
-     * @return a random number
+     * @param x specified x position 
+     * @return true if the turn is good otherwise false
      */
-    private DataTransport createRandomTurn(final GameState field, final int first, final int second)
+    private boolean checkNumberIsIntelligence(final GameState field, final int x)
     {
-        int breaker = 0;
-        do
+        int yt = 0;
+        for (; yt < field.getYsize(); yt++)
         {
-            int tmp;
-            if (first < 0)
+            if (isEmptyStone(field, yt, x))
             {
-                tmp = random(field);
+                break;
+            }
+        }
+        if (yt == field.getYsize() - 1)
+        {
+            // we are on top of the game field. There is nothing to check
+            return true;
+        }
+        final int y = yt;// we have to check on the next y-level
+
+        int c1 = 0;
+        for (int xx = x - 1; (xx > x - 4) && (xx >= 0); xx--)
+        {
+            if (isThisStone(field, State.MINE, y + 1, xx))
+            {
+                c1++;
             }
             else
             {
-                tmp = random(first, second);
+                break;
             }
-            final int x = tmp;
-            int yt = 0;
-            for (; yt < field.getYsize(); yt++)
-            {
-                if (isEmptyStone(field, yt, x))
-                {
-                    break;
-                }
-            }
-            if (yt == field.getYsize() - 1)
-            {
-                // we are on top of the game field. There is nothing to check
-                return new DataTransport(x);
-            }
-            final int y = yt + 1;// we have to check on the next y-level
+        }
 
-            int c1 = 0;
-            for (int xx = x - 1; (xx > x - 4) && (xx >= 0); xx--)
+        int c2 = 0;
+        for (int xx = x + 1; (xx < field.getXsize()) && (xx < x + 4 - c1); xx++)
+        {
+            if (isThisStone(field, State.MINE, y + 1, xx))
             {
-                if (isThisStone(field, State.MINE, y, xx))
+                c2++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (c1 + c2 + 1 == 4)
+        {
+            // random number is bad, because the human can win in the next turn
+            // return false; ToDo
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            // i = 0 means positive gradient 
+            // i = 1 means negative gradient
+            c1 = 0;
+            c2 = 0;
+
+            int xx;
+            if (i == 0)
+            {
+                xx = x - 1;
+            }
+            else
+            {
+                xx = x + 1;
+            }
+            for (int yy = y; (yy > y - 4) && (yy >= 0) && (xx > 0) && (xx < field.getXsize()); yy--)
+            {
+                if (isThisStone(field, State.MINE, yy, xx))
                 {
                     c1++;
                 }
@@ -197,12 +242,27 @@ public class Ghost
                 {
                     break;
                 }
+                if (i == 0)
+                {
+                    xx--;
+                }
+                else
+                {
+                    xx++;
+                }
             }
 
-            int c2 = 0;
-            for (int xx = x + 1; (xx < field.getXsize()) && (xx < x + 4 - c1); xx++)
+            if (i == 0)
             {
-                if (isThisStone(field, State.MINE, y, xx))
+                xx = x + 1;
+            }
+            else
+            {
+                xx = x - 1;
+            }
+            for (int yy = y + 2; (yy < field.getYsize()) && (yy < y + 5 - c1) && (xx > 0) && (xx < field.getXsize()); yy++)
+            {
+                if (isThisStone(field, State.MINE, yy, xx))
                 {
                     c2++;
                 }
@@ -210,25 +270,75 @@ public class Ghost
                 {
                     break;
                 }
+                if (i == 0)
+                {
+                    xx++;
+                }
+                else
+                {
+                    xx--;
+                }
             }
             if (c1 + c2 + 1 == 4)
             {
                 // random number is bad, because the human can win in the next turn
-                continue;
+                return false;
             }
-            // common statemant
-            breaker++;
-            return new DataTransport(x);
         }
-        while (breaker < 50);
-        System.out.println("schlaufe wurde abgebrochen");
+        return true;
+    }
+
+    /**
+     * Create a random number from two given possibilities. When the random turn
+     * provide a win for the human in the next turn, it will calculate an other
+     * random number.
+     *
+     * @author Yves Studer
+     * @param field representation of the current field
+     * @param first first number
+     * @param second second number
+     * @return a random number
+     */
+    private DataTransport createRandomTurn(final GameState field, final int first, final int second)
+    {
         if (first < 0)
         {
-            return new DataTransport(random(field));
+            int r;
+            int i = 0;
+            boolean b = false;
+            do
+            {
+                r = random(field);
+                b = checkNumberIsIntelligence(field, r);
+                i++;
+            }
+            while ((i < 50) && !b);
+            return new DataTransport(r);
         }
         else
         {
-            return null;
+            if (checkNumberIsIntelligence(field, first))
+            {
+                if (checkNumberIsIntelligence(field, second))
+                {
+                    return new DataTransport(random(first, second));
+                }
+                else
+                {
+                    return new DataTransport(first);
+                }
+            }
+            else
+            {
+                if (checkNumberIsIntelligence(field, second))
+                {
+                    return new DataTransport(second);
+                }
+                else
+                {
+                    return null;
+                }
+            }
         }
     }
 
@@ -236,6 +346,7 @@ public class Ghost
      * Create a random number. When the random turn provide a win for the human
      * in the next turn, it will calculate an other random number.
      *
+     * @author Yves Studer
      * @param field representation of the current field
      * @return a random number
      */
@@ -285,7 +396,14 @@ public class Ghost
                         if (isEmptyStone(field, yy + 1, x))
                         {
                             // the third stone is free
-                            return new DataTransport(x);
+                            if (checkNumberIsIntelligence(field, x))
+                            {
+                                return new DataTransport(x);
+                            }
+                            else
+                            {
+                                return null;
+                            }
                         }
                     }
                 }
@@ -365,14 +483,28 @@ public class Ghost
                             }
                             else
                             {
-                                return new DataTransport(second);
+                                if (checkNumberIsIntelligence(field, second))
+                                {
+                                    return new DataTransport(second);
+                                }
+                                else
+                                {
+                                    return null;
+                                }
                             }
                         }
                         else
                         {
                             if (isEmptyStone(field, y - 1, second))
                             {
-                                return new DataTransport(first);
+                                if (checkNumberIsIntelligence(field, first))
+                                {
+                                    return new DataTransport(first);
+                                }
+                                else
+                                {
+                                    return null;
+                                }
                             }
                             else
                             {
